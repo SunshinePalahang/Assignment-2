1dd4c91482eafa6010a3368d2b17fb2083af3f64
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-10-26 12:28:09 +0800

    Second commit

---
+++ program2.py
@@ -0,0 +1,11 @@
+apple = int(input("How many apples? "))
+orange = int(input("How many oranges? "))
+
+apple_price = 20
+orange_price = 25
+
+apple_total = apple*apple_price
+orange_total= orange*orange_price
+total= apple_total+orange_total
+
+print(f"The total amount is {total}.")
